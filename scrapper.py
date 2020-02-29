aab8927932bfc4c0207fb5f54eff443a41cad764
vk23208 <31855976+vk23208@users.noreply.github.com>
2020-03-01 03:19:15 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -45,14 +45,14 @@ def send_mail1():
     server.starttls()
     server.ehlo()
     
-    server.login('vk23208@gmail.com' , 'xgbvqmuognavfvsg')
+    server.login('your gmail id' , 'your 3rd part apps password')
     
     subject = 'Price fell down RealmeX2Pro 8GB'
     body = '''Check flipkart link   https://www.flipkart.com/realme-x2-pro-neptune-blue-128-gb/p/itma3203d88190a3?pid=MOBFM2WZNK2HHZCM&lid=LSTMOBFM2WZNK2HHZCMBBWTEO&marketplace=FLIPKART&srno=s_1_3&otracker=search&otracker1=search&fm=SEARCH&iid=7154358a-5a23-4084-ac57-27ec3c3fdad0.MOBFM2WZNK2HHZCM.SEARCH&ppt=sp&ppn=sp&ssid=176gzseyn40000001581822840348&qH=5fb8821185bcc702'''
     msg = f"Subject : {subject}\n\n{body}"
     
     #i am using my own inbox for sending and receiving mails for now, you can use your own
-    server.sendmail('vk23208@gmail.com','vk23208@gmail.com',msg)
+    server.sendmail('your gmail id','whome to send',msg)
     print('Hey, Email has been sent for RealmeX2pro 8GB!')
     server.quit()
     
@@ -62,13 +62,13 @@ def send_mail2():
     server.starttls()
     server.ehlo()
     
-    server.login('vk23208@gmail.com' , 'xgbvqmuognavfvsg')
+    server.login('your gmail id' , 'your 3rd part apps password')
     
     subject = 'Price fell down RealmeX2Pro 6GB'
     body = '''https://www.flipkart.com/realme-x2-pro-lunar-white-64-gb/p/itma3203d88190a3?pid=MOBFM2WZVWUNZGTX&lid=LSTMOBFM2WZVWUNZGTXUXZQFO&marketplace=FLIPKART&sattr[]=color&sattr[]=storage&sattr[]=ram&st=ram&otracker=search'''
     msg = f'Subject : {subject}\n\n{body}'
     
-    server.sendmail('vk23208@gmail.com','vk23208@gmail.com',msg)
+    server.sendmail('your gmail id','whome to send',msg)
     print('Hey, Email has been sent for RealmeX2pro 6GB!')
     server.quit()
     
